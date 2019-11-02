81c0cfcc350ccd788ac1da30758f6867494a4d2e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-11-02 07:52:13 +0530

    Add rollup config.

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import buble from 'rollup-plugin-buble';
+import {terser} from 'rollup-plugin-terser';
+
+export default {
+    input: './src/replace.js',
+    output: {
+        format: 'iife',
+        file: './buzzword.js',
+        indent: false
+    },
+    plugins: [resolve(), commonjs(), terser(), buble()]
+};
